3fcb975310a4475394c8b8a90f30a0bbe94f081a
Sabino García Palechor <sabino.garcia.palechor@gmail.com>
2022-11-26 22:18:05 -0300

    empezando con js

--- validate.js
+++ validate.js
@@ -1,1 +1,11 @@
 //Seu JavaScript de validação aqui
+
+const botonEnviar = document.querySelector('[data-boton]');
+
+const enviarDatos=(evento)=>{
+    evento.preventDefault();
+    const txtNombre = document.querySelector("[data-nombre]");
+    alert(txtNombre.value);
+}
+
+botonEnviar.addEventListener('click', enviarDatos);
